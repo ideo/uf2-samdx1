bc0f5f2b400be904a33c5ea686c10fca57c8e321
Derek Olson <derek@derek-o.com>
2022-05-31 16:35:29 -0400

    update eyeo flower board config

--- boards/eyeo_flower/board_config.h
+++ boards/eyeo_flower/board_config.h
@@ -3,14 +3,14 @@
 
 #define CRYSTALLESS    1
 
-#define VENDOR_NAME "Adafruit Industries"
-#define PRODUCT_NAME "Trinket M0"
-#define VOLUME_LABEL "TRINKETBOOT"
-#define INDEX_URL "http://adafru.it/3500"
-#define BOARD_ID "SAMD21E18A-Trinket-v0"
+#define VENDOR_NAME "IDEO"
+#define PRODUCT_NAME "Eyeo Flower"
+#define VOLUME_LABEL "FLOWERBOOT"
+#define INDEX_URL "https://github.com/ideo/eyeo-flower"
+#define BOARD_ID "SAMD21E17D-Flower-v0"
 
-#define USB_VID 0x239A
-#define USB_PID 0x001E
+#define USB_VID 0x1209
+#define USB_PID 0xE7E0
 
 #define LED_PIN PIN_PA10
 //#define LED_TX_PIN PIN_PA27
@@ -20,6 +20,5 @@
 #define BOARD_RGBLED_DATA_PIN             PIN_PA00
 
 #define FLASH_NUM_ROWS 512
-#define CRYSTALLESS 1
 
 #endif
